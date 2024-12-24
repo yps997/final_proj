1f78aa3994531ee88232ca234d84e6f9ac8dbdab
yps997 <yairps997@gmail.com>
2024-12-24 17:35:25 +0200

    add admin new news

---
+++ new_news_service/app/kafka_settings/admin.py
@@ -0,0 +1,27 @@
+import os
+from kafka import KafkaAdminClient
+from kafka.admin import NewTopic
+from kafka.errors import TopicAlreadyExistsError
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+def init_topics():
+    client = KafkaAdminClient(bootstrap_servers=os.environ['BOOTSTRAP_SERVERS'])
+    topics = [os.environ['TOPIC_NEWS_DATA']]
+
+    topics = [NewTopic(
+        name=topic,
+        num_partitions=int(os.environ['NUM_PARTITIONS']),
+        replication_factor=int(os.environ['NUM_REPLICATIONS'])
+    ) for topic in topics]
+
+    try:
+        client.create_topics(topics)
+    except TopicAlreadyExistsError as e:
+        print(str(e))
+    finally:
+        client.close()
+
+if __name__ == '__main__':
+    init_topics()
